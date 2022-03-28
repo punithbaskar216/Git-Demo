ef416136c6a394944bc2aa89435994402c9d355a
unknown <punithbaskar216@gmail.com>
2022-03-28 12:57:15 +0530

    added git-demo project to local repository

---
+++ src/main/java/com/springboot/git_demo/JavaTechieSpringBootGitDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.git_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechieSpringBootGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTechieSpringBootGitDemoApplication.class, args);
+		System.out.println("Spring Boot Demo...");
+	}
+
+}
